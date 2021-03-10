b2801ecd0ea8ca053113bc58647e73c0afc8db8b
saqibsaud <saqibsaudc@gmail.com>
2021-03-10 12:41:34 +0530

    cake actions and reducer

---
+++ src/redux/cakes/cakeReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    numOfCakes: 10
+}
+
+export const cakeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'BUY_CAKE': return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        
+        default: return state
+    }
+}
