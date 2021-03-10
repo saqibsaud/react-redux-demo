cb9543d826e9a6e17808794ebef99eda2fd94f72
saqibsaud <saqibsaudc@gmail.com>
2021-03-10 12:56:00 +0530

    Using action payload to pass dynamic values

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
+import NewCakeContainer from './components/NewCakeContainer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <HooksCakeContainer />
         <CakeContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
--- src/redux/cakes/cakeReducer.js
+++ src/redux/cakes/cakeReducer.js
@@ -6,7 +6,7 @@ export const cakeReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'BUY_CAKE': return {
             ...state,
-            numOfCakes: state.numOfCakes - 1
+            numOfCakes: state.numOfCakes - action.payload
         }
         
         default: return state
