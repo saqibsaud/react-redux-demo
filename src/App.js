2d9876fe9acb41b314c034e4a3e10ddbc3755463
saqibsaud <saqibsaudc@gmail.com>
2021-03-10 18:15:27 +0530

    Item container as passing parent pros

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
+        <IceCreamContainer /> 
         <NewCakeContainer />
       </div>
     </Provider>
